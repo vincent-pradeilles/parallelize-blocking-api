ee26ea6f43322187992edfdc24191a3a1a02a399
Vincent Pradeilles <little.facto@gmail.com>
2017-07-25 11:39:32 +0200

    Update ParallelOrchestration.swift

--- ParallelOrchestration.swift
+++ ParallelOrchestration.swift
@@ -12,8 +12,7 @@ import RxSwift
 struct ParallelOrchestration {
     static func getShippingStatus(for user: User) -> Observable<ShippingStatus> {
         return AsyncDAOFacade.getOrderIds(for: user)
-            // calls to AsyncDAOFacade.getOrder are expensive, so we dispatch each of them to their own threads
-            .flatMap(AsyncDAOFacade.getOrder).observeOn(ConcurrentDispatchQueueScheduler(qos: .userInitiated))
+            .flatMap(AsyncDAOFacade.getOrder)
             .flatMap { (order) in
                return Observable.from(order.products).flatMap { AsyncDAOFacade.getStatus(for: $0, in: order) }
             }
